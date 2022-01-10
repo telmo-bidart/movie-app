a480882b590a3cc999e53561528621ac7cfa2ca2
telmo bidart <telmobidart@gmail.com>
2022-01-10 11:53:27 -0300

    feat(css): added style

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Movie } from "./components/Movie";
 import "./App.css";
 
 const FEATURED_API =
-  "https://api.themoviedb.og/3/discover/movie?sort_by=popularity.desc&api_key=942277627f5c059768e1e4eda4c49345&page=1";
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=942277627f5c059768e1e4eda4c49345&page=1";
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_API =
   "https://api.themoviedb.org/3/search/movie?api_key=942277627f5c059768e1e4eda4c49345&query=";
@@ -11,17 +11,21 @@ const SEARCH_API =
 function App() {
   const [movies, setMovies] = useState([]);
 
-  useEffect(async () => {
-    const moviesResp = await fetch(FEATURED_API);
-    const movies = await moviesResp.json();
-
-    setMovies(movies);
+  useEffect(() => {
+    fetch(FEATURED_API)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      setMovies(data.results);
+    });
   }, []);
   return (
-    <div className="App">
-      {movies.map((movie) => (
-        <Movie />
-      ))}
+    <div className="movie-container">
+      <header>
+        <input className="input" type="text" placeholder="Search..." />
+      </header>
+      {movies.length > 0 && movies.map((movie) =>
+       <Movie key={movie.id} {...movie} />)}
     </div>
   );
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,22 @@
-export const Movie = () => {
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+export const Movie = ({ title, poster_path, overview, vote_average }) => {
     return (
         <div className='movie'>
-            aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+            <img src={IMG_API + poster_path} alt={title} />
+            <div className="movie-info">
+                <h3>
+                    {title}
+                </h3>
+                <span>
+                    {vote_average}
+                </span>
+            </div>
+            <div className="movie-over">
+                <h2>
+                    Overview:
+                </h2>
+                <p> {overview} </p>
+            </div>
         </div>
     )
 }
